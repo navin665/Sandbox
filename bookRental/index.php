14d1ad5da78a14ce948ff1c3b2b96ca29b3a6101
navin665 <navin.iem@gmail.com>
2017-10-04 19:57:57 +0530

    InitialCommit

---
+++ bookRental/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Books Rentals</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script src="javascript/script.js"></script>
+</head>
+<body>
+	<div>hello</div>
+</body>
+</html>
